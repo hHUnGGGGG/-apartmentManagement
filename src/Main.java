cab1f7f3b67b27929822fe258c6a31f550e64a69
vhvhvh <viethoanghay123@gmail.com>
2024-11-05 21:28:55 +0700

    hoang update
    xoa het

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,5 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) {
-        // Tạo một nút
-        Button button = new Button("Click Me!");
-
-        // Thêm hành động cho nút khi nhấn
-        button.setOnAction(event -> {
-            System.out.println("Button was clicked!");
-        });
-
-        // Tạo một layout và thêm nút vào
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-
-        // Tạo một scene
-        Scene scene = new Scene(layout, 300, 200);
-
-        // Thiết lập stage
-        primaryStage.setTitle("JavaFX Button Example");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+   
 }
