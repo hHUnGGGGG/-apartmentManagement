95e3e0f33db270b838db52d63d7ea6df9621857d
Tú Hoàng <ttien@gmail.com>
2024-11-11 01:05:03 +0700

    sqlserver -> mysql + dùng railway(cloud)

--- src/Test.java
+++ src/Test.java
@@ -13,14 +13,14 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("A");
 		Connection conn = DatabaseConnection.getConnection();
-		String sql = "SELECT *  FROM NHANKHAU ";
+		String sql = "SELECT *  FROM HOKHAU ";
 		try {
 			PreparedStatement statement = conn.prepareStatement(sql);
 			ResultSet resultSet = statement.executeQuery();
 			
 			System.out.println("A");
 			while(resultSet.next()) {
-				System.out.println("Mã nhân khẩu: " + resultSet.getInt("MANHANKHAU") );
+				System.out.println("Mã chủ hộ: " + resultSet.getInt("MACHUHO") );
 			}
 		}catch (Exception e) {
 			e.printStackTrace();
