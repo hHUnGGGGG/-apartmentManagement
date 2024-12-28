bb362a11811b077606e85799a047f6d09a7ada99
KHẢI <nguyenkhai131104@gmail.com>
2024-12-29 00:38:36 +0700

    user

--- src/Service/HoKhauUserService.java
+++ src/Service/HoKhauUserService.java
@@ -1,13 +1,13 @@
 package Service;
 
-import Models.HoKhauChuHoModel;
-import Models.HoKhauModel;
+
 import Models.NhanKhauModel;
 import database.DatabaseConnection;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NavigableMap;
 
 public class HoKhauUserService {
     private final Connection connection;
@@ -19,7 +19,7 @@ public class HoKhauUserService {
     public List<NhanKhauModel> getListNhanKhau(int maHoKhau) {
         List<NhanKhauModel> danhSachNhanKhau = new ArrayList<>();
 
-        String query = "SELECT * FROM NHANKHAU WHERE MAHOKHAU = ?";
+        String query =  "SELECT * FROM NHANKHAU WHERE MAHOKHAU = ?";
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             // Gán tham số cho câu lệnh SQL
@@ -39,16 +39,83 @@ public class HoKhauUserService {
         return danhSachNhanKhau;
     }
 
+    public List<NhanKhauModel> searchNKbyTen(int maHoKhau, String ten){
+        List<NhanKhauModel> listNhanKhau = new ArrayList<>();
+        String query = "SELECT * FROM NHANKHAU WHERE MAHOKHAU = ? AND LOWER(HOTEN) LIKE LOWER(?)";
+        String searchPattern = "%" + ten.trim() + "%";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setInt(1,maHoKhau);
+            preparedStatement.setString(2,searchPattern);
+
+            try(ResultSet rs = preparedStatement.executeQuery()){
+                while (rs.next()){
+                    NhanKhauModel nhankhau = new NhanKhauModel(
+                            rs.getInt("MANHANKHAU"),
+                            rs.getString("CCCD"),
+                            rs.getString("HOTEN"),
+                            rs.getDate("NGAYSINH"),
+                            rs.getString("SDT"),
+                            rs.getInt("MAHOKHAU"),
+                            rs.getString("QUANHEVOICHUHO"),
+                            rs.getString("TRANGTHAI"),
+                            rs.getInt("SOPHONG"),
+                            rs.getBoolean("TRANGTHAITAMVANG")
+                    );
+
+                    listNhanKhau.add(nhankhau);
+                }
+            }
+        } catch (SQLException e){
+            System.err.println("Lỗi khi tìm kiếm nhân khẩu theo tên:" + e.getMessage());
+        }
+        return listNhanKhau;
+    }
+
+    public List<NhanKhauModel> searchNKbyCCCD(int maHoKhau, String cccd){
+        List<NhanKhauModel> listNhanKhau = new ArrayList<>();
+        String query = "SELECT * FROM NHANKHAU WHERE MAHOKHAU = ? AND CCCD LIKE ?";
+        String searchPattern = "%" + cccd.trim() + "%";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setInt(1,maHoKhau);
+            preparedStatement.setString(2,searchPattern);
+
+            try(ResultSet rs = preparedStatement.executeQuery()){
+                while (rs.next()){
+                    NhanKhauModel nhankhau = new NhanKhauModel(
+                            rs.getInt("MANHANKHAU"),
+                            rs.getString("CCCD"),
+                            rs.getString("HOTEN"),
+                            rs.getDate("NGAYSINH"),
+                            rs.getString("SDT"),
+                            rs.getInt("MAHOKHAU"),
+                            rs.getString("QUANHEVOICHUHO"),
+                            rs.getString("TRANGTHAI"),
+                            rs.getInt("SOPHONG"),
+                            rs.getBoolean("TRANGTHAITAMVANG")
+                    );
+
+                    listNhanKhau.add(nhankhau);
+                }
+            }
+        } catch (SQLException e){
+            System.err.println("Lỗi khi tìm kiếm nhân khẩu theo CCCD:" + e.getMessage());
+        }
+        return listNhanKhau;
+    }
+
     private NhanKhauModel createNhanKhauFromResultSet(ResultSet rs) throws SQLException {
         return new NhanKhauModel(
-                rs.getInt("MAHOKHAU"),
                 rs.getInt("MANHANKHAU"),
                 rs.getString("CCCD"),
                 rs.getString("HOTEN"),
                 rs.getDate("NGAYSINH"),
                 rs.getString("SDT"),
+                rs.getInt("MAHOKHAU"),
                 rs.getString("QUANHEVOICHUHO"),
-                rs.getBoolean("TRANGTHAI")
+                rs.getString("TRANGTHAI"),
+                rs.getBoolean("TRANGTHAITAMVANG")
         );
     }
 }
